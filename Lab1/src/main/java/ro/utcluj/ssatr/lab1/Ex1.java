98af5dff694f1be0c4068de5a1893c156b56e088
StoianAdrian <madalin32@yahoo.com>
2020-10-12 20:21:23 +0300

    Lab1

---
+++ Lab1/src/main/java/ro/utcluj/ssatr/lab1/Ex1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.utcluj.ssatr.lab1;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println("Exercitiul 1");
+    }
+}
+
