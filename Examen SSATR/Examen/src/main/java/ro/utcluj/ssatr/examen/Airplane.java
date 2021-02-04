dab6250671df9f8fb8c3470eec13847abbd0be56
StoianAdrian <madalin32@yahoo.com>
2021-02-04 12:10:34 +0200

    Examen SSATR with internal files

---
+++ Examen SSATR/Examen/src/main/java/ro/utcluj/ssatr/examen/Airplane.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.utcluj.ssatr.examen;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Airplane {
+    private int number;
+    private int LandingTime;
+
+    public Airplane(int number) {
+        this.number = number;
+    }
+    
+    public Airplane(int number, int LandingTime) {
+    
+        this.number = number;
+        this.LandingTime = LandingTime;
+    }
+    
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int geLandingTime() {
+        return LandingTime;
+    }
+
+    public void setLandingTime(int LandingTime) {
+        this.LandingTime = LandingTime;
+    }
+
+}
+
---
+++ Examen SSATR/Examen/src/main/java/ro/utcluj/ssatr/examen/ClientAirplane.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.utcluj.ssatr.examen;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import static java.lang.Integer.parseInt;
+import org.apache.commons.lang3.RandomStringUtils;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author lenovo
+ */
+public class ClientAirplane extends Thread {
+    BufferedReader fluxIn;
+    PrintWriter fluxOut;
+
+
+    public ClientAirplane() throws IOException {
+        Socket s = new Socket("127.0.0.1", 7600);
+        fluxIn = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        fluxOut = new PrintWriter(new OutputStreamWriter(s.getOutputStream()), true);
+    }
+
+    private Airplane initAirplane() {
+        int NumberAirplane = parseInt(RandomStringUtils.randomNumeric(3));
+ 
+        return new Airplane(NumberAirplane);
+    }
+    private int getAirplaneString(Airplane Air) {
+        return Air.getNumber();
+    }
+
+    public void run()
+    {
+        while (true) {
+            String line;
+            try {
+                Airplane Air = initAirplane();
+                int Airplane = getAirplaneString(Air);
+                fluxOut.println(Airplane);
+                line = fluxIn.readLine();
+                StringTokenizer st = new StringTokenizer(line);
+                String LandingTime = st.nextToken();
+                String Piste = st.nextToken();
+                //TODO: add platform where Airplane stops
+                if (parseInt(LandingTime) / 500 != 0) {
+                    System.out.println( Air.getNumber() + " a aterizat pe " + Piste + "! Acesta va stationa " + parseInt(LandingTime) / 500 + " minute!");
+                } else {
+                    System.out.println( Air.getNumber() + " nu va ateriza");
+                }
+                try {
+                    Thread.sleep(parseInt(LandingTime));
+                    if (parseInt(LandingTime) / 500 != 0) {
+                        System.out.println(Air.getNumber() + " a decolat");
+                    } else {
+                        System.out.println(Air.getNumber() + " nu a aterizat!");
+                    }
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(ClientAirplane.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                fluxOut.println("Decolare de la pista " + Piste);
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(ClientAirplane.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(ClientAirplane.class.getName()).log(Level.SEVERE, null, ex);
+                break;
+            }
+        }
+    }
+
+    public void sendMessage(String msg) {
+        fluxOut.println(msg);
+    }
+
+   public static void main(String[] args) throws IOException {
+    ClientAirplane Plane = new ClientAirplane();
+    Plane.start();
+   
+   
+   }
+}
+
+
